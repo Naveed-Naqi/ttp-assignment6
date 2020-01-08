c14753d2b870041a6d2047cacd5d1b522877643c
Naveed Naqi <nnaqi5534@gmail.com>
2020-01-08 15:59:20 -0500

    finished project 1

---
+++ src/components/SingleContactCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    width: "30%",
+    textAlign: "center",
+    display: "inline-block",
+    margin: "1%",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function SingleContactCard({city, state, lat, long, pop}) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card} variant="outlined">
+      <CardContent>
+        <Typography variant="h5" component="h2" className={classes.pos}>
+            {city + ", " + state}
+        </Typography>
+        <Typography className={classes.pos} >
+            Location: {lat + ", " + long}
+        </Typography>
+        <Typography variant="body2" component="p">
+            Estimated Population: {pop}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
--- src/components/ZipCode.js
+++ src/components/ZipCode.js
@@ -1,10 +1,66 @@
 import React, { Component } from 'react'
+import axios from "axios";
+import SingleContactCard from './SingleContactCard';
 
 export default class ZipCode extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            zipcode: "",
+            data: [],
+            0: ""
+        };
+    } 
+
+    handleChange = (event) => {
+
+        //This first part is setting the state like normal.
+        this.setState({
+            [event.target.id]: event.target.value
+        }, () => {
+            axios.get(`http://ctp-zip-api.herokuapp.com/zip/` + this.state.zipcode)
+            .then(response => this.setState({data: response.data}))
+            .catch(err => {})
+        });
+    }
+
+    determineDisplay = () => {
+
+        if(Object.keys(this.state.data).length === 0) {
+            return (
+                <h1>Nothing Found</h1>
+            );
+        } else {
+            return(
+                <div>
+                    {this.state.data.map( (elem) => {
+                        return (
+                            <SingleContactCard
+                                key={elem.City+elem.Lat+elem.Long}
+                                city={elem.City}
+                                state={elem.State}
+                                lat={elem.Lat}
+                                long={elem.Long}
+                                pop={elem.EstimatedPopulation}
+                            ></SingleContactCard>
+                        );
+                    })}
+                </div>
+            );
+        }
+    }
+
+
     render() {
+
+        let citiesDisplay = this.determineDisplay();
+
         return (
             <div>
-                <input type="text" placeholder="zip code"></input>
+                <input id="zipcode" type="text" placeholder="zip code" onChange={this.handleChange}></input>
+                {citiesDisplay}
             </div>
         )
     }
