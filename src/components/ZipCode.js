82fe59114c32b3fe90cf0db627ec21c06c97855e
Naveed Naqi <nnaqi5534@gmail.com>
2020-01-08 14:48:53 -0500

    created zip code

---
+++ src/components/ZipCode.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ZipCode extends Component {
+    render() {
+        return (
+            <div>
+                <input type="text" placeholder="zip code"></input>
+            </div>
+        )
+    }
+}
